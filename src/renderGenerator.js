59fc77658df13c4a31e32b3264f732ebe192b2ff
Dan Neame <danneame@gmail.com>
2014-06-22 20:39:54 +0100

    Removed POBF picture and minus points

--- src/renderGenerator.js
+++ src/renderGenerator.js
@@ -157,7 +157,9 @@ module.exports = {
                         buddy: _userBuddies.indexOf(comment.postedby) > -1,
                         ignored: _userIgnores.indexOf(comment.postedby) > -1,
                         toggleSourceLabel: (comment.postedby === user.username && moment(comment.created).diff(new Date())>-600000) ? 'Edit Post' : 'View Source',
-                        editPercent: Math.floor(comment.edit_percent)
+                        editPercent: Math.floor(comment.edit_percent),
+                        haspoints: comment.points > 0,
+                        hasmultiplepoints: comment.points > 1
                     });
                 })
             }));
