2c6c1777a6f95ecd697e70655e2b15a7ade93430
Dan Neame <danneame@gmail.com>
2014-07-06 18:21:13 +0100

    Error page improvements

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -2,19 +2,21 @@ var api = require('./api'),
     renderGenerator = require('./renderGenerator'),
     errorDungeon = [
         [5, 1, 1, 0, 0],
-        [0, 1, 1, 0, 0],
-        [0, 0, 1, 1, 2],
-        [4, 1, 1, 1, 0],
+        [0, 1, 7, 0, 0],
+        [0, 7, 8, 7, 2],
+        [4, 1, 7, 1, 0],
         [0, 3, 0, 1, 6]
     ],
     tileDescriptions = [
         'You have somehow found yourself in the void. You found an error in the error reporting page. Well done. I hope you\'re happy now.',
-        'You are in a dark forest.',
-        'You enter a clearing. There is a small shrine to the Jakes here.',
+        'You are in a dark forest. It is very dark. There are some trees here.',
+        'You enter a clearing. There is a small shrine to the Jakes here. Someone has left their t-shirt next to it.',
         'You find a strange box with flashing lights. The box is in a very poor state indeed. It has \'Yayhooray.net\' written on the side.',
-        'You find a small cave. This is where matty comes to cry.',
-        'You have found the exit! This will have rebooted the server and fixed the error. Just kidding, you\'re still in the forest. A bird flies past and poos on your shoulder.',
-        'You find yourself at the entrance to a dark forest.'
+        'You find a small cave. This is where matty comes to cry. The air is thick with discarded emotions.',
+        'You have found the exit! Just kidding, you\'re still in the forest. A bird flies past and poos on your shoulder.',
+        'You find yourself at the entrance of a dark forest.',
+        'You are deep within the forest. There are like loads of trees and toadstools growing on stuff.',
+        'You are deep within the forest. Cages containing skeletons are hanging from high up in the canopy. A sign reads "Criminals and Permabanned".'
     ];
 
 function getTile(x, y){
