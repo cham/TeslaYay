6c645dd9b494f1deb7f1e1b6f2875a45a4569a30
Dan <daniel@sibitzky.com>
2023-07-10 07:33:08 -0400

    For docker

--- worker.js
+++ worker.js
@@ -14,7 +14,7 @@ var express = require('express'),
     path = require('path'),
     sessionGenerator = require('./src/sessionGenerator'),
     sessionStore = new RedisStore({
-        host: 'localhost',
+        host: process.env.REDIS_HOST || 'localhost',
         port: 6379,
         db: 2
     }),
